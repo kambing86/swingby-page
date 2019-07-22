88f4ed427b619cae16751e2645f5535c9cf6c0f5
Chua Kang Ming <kambing860210@gmail.com>
2019-07-22 20:11:30 +0800

    add react-router-dom and put in router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router basename={process.env.PUBLIC_URL || ""}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
     </div>
   );
 };
