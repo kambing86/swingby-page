e19d4d82d74262ed9a9ff02e2c948e847c00ed51
Chua Kang Ming <kambing860210@gmail.com>
2019-07-29 21:48:12 +0800

    fix image path

--- src/pages/Home/Parallax.tsx
+++ src/pages/Home/Parallax.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { useWindowScroll } from "react-use";
 
+const { PUBLIC_URL } = process.env;
+
 export default () => {
   const { y } = useWindowScroll();
   return (
     <>
       <div
         className="header-star"
-        style={{ transform: `translate(0, ${0.2 * y}px)` }}
+        style={{
+          backgroundImage: `url("${PUBLIC_URL}/assets/HEADER02/star.png")`,
+          transform: `translate(0, ${0.2 * y}px)`,
+        }}
       />
       <div
         className="header-orbit"
-        style={{ transform: `translate(0, ${0.3 * y}px)` }}
+        style={{
+          backgroundImage: `url("${PUBLIC_URL}/assets/HEADER02/orbit.png")`,
+          transform: `translate(0, ${0.3 * y}px)`,
+        }}
       />
       <div
         className="header-bg"
-        style={{ transform: `translate(0, ${0.4 * y}px)` }}
+        style={{
+          backgroundImage: `url("${PUBLIC_URL}/assets/HEADER02/bg.png")`,
+          transform: `translate(0, ${0.4 * y}px)`,
+        }}
       />
     </>
   );
