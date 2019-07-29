f03eac60732e08dff4c3d52bbc6859f552869376
Chua Kang Ming <kambing860210@gmail.com>
2019-07-29 20:58:53 +0800

    fix link

--- src/pages/Home/Header.tsx
+++ src/pages/Home/Header.tsx
@@ -13,10 +13,10 @@ export default () => {
         <img src="/assets/HEADER02/LOGO.png" alt="LOGO" />
       </a>
       <div className="top-menu d-none d-md-block">
-        <a href="#/Product">PRODUCT</a>
-        <Link to="https://medium.com/swingby-protocol/">BLOG</Link>
-        <Link to="https://github.com/swingbyprotocol/">GITHUB</Link>
-        <Link to="https://twitter.com/swingbyprotocol/">TWITTER</Link>
+        <Link to="/Product">PRODUCT</Link>
+        <a href="https://medium.com/swingby-protocol/">BLOG</a>
+        <a href="https://github.com/swingbyprotocol/">GITHUB</a>
+        <a href="https://twitter.com/swingbyprotocol/">TWITTER</a>
       </div>
       <div
         style={{
