4504a5235a08efa23a790d08277cef5fe99fa954
Chua Kang Ming <kambing860210@gmail.com>
2019-08-03 05:31:21 +0800

    use another package for in view and fix test

--- src/components/pages/Home/Services/ServiceContainer.tsx
+++ src/components/pages/Home/Services/ServiceContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, ReactNode } from "react";
+import { useInView } from "react-intersection-observer";
 import { animated, useSpring } from "react-spring";
-import useIsInViewport from "use-is-in-viewport";
 
 export default (props: {
   imageSrc: string;
@@ -9,21 +9,21 @@ export default (props: {
   titleContent: ReactNode;
   description: ReactNode;
 }) => {
-  const [isInViewport, childElToWatch] = useIsInViewport({
-    threshold: 50,
+  const [ref, isInView] = useInView({
+    threshold: 0.5,
   });
   const [springProps, set] = useSpring(() => ({
     transform: "translate(0, -50px)",
     opacity: 0,
   }));
   useEffect(() => {
-    if (isInViewport) {
+    if (isInView) {
       set({ transform: "translate(0, 0)", opacity: 1 });
     }
-  }, [isInViewport, set]);
+  }, [isInView, set]);
   return (
     <animated.div style={springProps}>
-      <div className="services-bg p-3" ref={childElToWatch}>
+      <div className="services-bg p-3" ref={ref}>
         <div className="container">
           <div className="row">
             <div className="col-12 col-md-6 px-3 my-3 my-md-4">
