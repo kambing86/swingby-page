2aa6c3b8fcae2fdd091dbbb65e3a76ed44f07ee1
Chua Kang Ming <kambing860210@gmail.com>
2019-08-03 06:47:54 +0800

    fix links in community

--- src/components/pages/Home/Community/Community.tsx
+++ src/components/pages/Home/Community/Community.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Community.module.scss";
 
+const { PUBLIC_URL } = process.env;
+
 export default () => (
   <div className={`${styles.background} text-secondary py-3`}>
     <div className={`${styles.header} my-5`}>
@@ -12,12 +14,12 @@ export default () => (
       Learn more about Maker, chat with the team, and have your say in shaping
       the future of commerce on the blockchain.
     </div>
-    <a href="/#Chat">Chat »</a>
-    <a href="/#Subreddit">Subreddit »</a>
-    <a href="/#Events">Events »</a>
+    <a href={`${PUBLIC_URL}/#Chat`}>Chat »</a>
+    <a href={`${PUBLIC_URL}/#Subreddit`}>Subreddit »</a>
+    <a href={`${PUBLIC_URL}/#Events`}>Events »</a>
     <a href="https://medium.com/swingby-protocol/">Blog »</a>
     <div className="my-5">
-      <a className="link-primary" href="/#Join">
+      <a className="link-primary" href={`${PUBLIC_URL}/#Join`}>
         <div>Join the community</div>
       </a>
     </div>
