19eec5e07e199343766e0bd7ac5175ffece386ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-30 02:33:36 +0000

    Deploy kambing86/swingby-page to github.com/kambing86/swingby-page.git:gh-pages

---
+++ precache-manifest.a6f64685801c87544685a6a858e30ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3ae7049b679e89f1ab23c510a551a2",
+    "url": "/swingby-page/index.html"
+  },
+  {
+    "revision": "a2d4861cc98b5297980e",
+    "url": "/swingby-page/static/css/main.ce61898f.chunk.css"
+  },
+  {
+    "revision": "fef2cb4bc10b8b2c78a4",
+    "url": "/swingby-page/static/js/2.9f1f7417.chunk.js"
+  },
+  {
+    "revision": "a2d4861cc98b5297980e",
+    "url": "/swingby-page/static/js/main.f872e838.chunk.js"
+  },
+  {
+    "revision": "8cf54a71379b139b4a61",
+    "url": "/swingby-page/static/js/runtime~main.94aa0c2c.js"
+  }
+]);
